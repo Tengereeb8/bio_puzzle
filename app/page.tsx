0ed4b0e922450eccbe882db111ac5790c3a42282
25lp3625 User <25lp3625@administrators-iMac-6.local>
2026-04-29 10:53:41 +0800

    ustgal

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import { getUsers } from "./actions/users";
 import App from "./components/App";
 
 export default async function Home() {
-  const users = await getUsers();
-
-  console.log(users);
   return (
     <div className="bg-white text-black h-fit flex justify-center items-center">
       <App />
     </div>
   );
 }
-//hello
